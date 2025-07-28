ac7e3e3059e9230459b4a7cfcdd59cf5294dd428
wiliamgomide <wiliampintogomide@gmail.com>
2025-07-28 09:36:38 -0300

    desafio finalizado.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-let nome = "felipe";
-console.log(nome);
+let nomeDoHeroi = "Haizemberg"
+console.log(nomeDoHeroi)
+
+let xpDoHeroi = 0; // Defina o valor de xpDoHeroi conforme necessário
+let nivelDoHeroi
+
+if (xpDoHeroi <= 1000) {
+  nivelDoHeroi = "Ferro"
+}
+
+if ((xpDoHeroi >= 1001) && (xpDoHeroi <= 2000)) {
+  nivelDoHeroi = "Bronze"
+}
+
+if ((xpDoHeroi >= 2001) && (xpDoHeroi <= 5000)) {
+  nivelDoHeroi = "Prata"
+}
+
+if ((xpDoHeroi >= 5001) && (xpDoHeroi <= 7000)) {
+  nivelDoHeroi = "Ouro"
+}
+
+if ((xpDoHeroi >= 7001) && (xpDoHeroi <= 8000)) {
+  nivelDoHeroi = "Platina"
+}
+
+if ((xpDoHeroi >= 8001) && (xpDoHeroi <= 9000)) {
+  nivelDoHeroi = "Ascendente"
+}
+
+if ((xpDoHeroi >= 9001) && (xpDoHeroi <= 10000)) {
+  nivelDoHeroi = "Imortal"
+}
